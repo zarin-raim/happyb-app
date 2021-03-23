503d26c7a55a89c50a54b820824e3a480e98b22b
Zarina Raimkulova <zarinar1105@gmail.com>
2021-03-23 12:14:39 +0100

    initial commit

---
+++ app/src/main/java/raim/zarin/happybirthday/MainActivity.kt
@@ -0,0 +1,11 @@
+package raim.zarin.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
